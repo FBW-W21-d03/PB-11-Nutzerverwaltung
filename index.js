48c43f440c6038e902d9d0640da90fa425075b25
Mohal1125 <rajwindersinghmohal@yahoo.com>
2021-11-16 11:32:15 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,20 @@
+let isFull = false;
+
+var users = ["John Smith" , "Bob Tyler"]
+
+if (isFull) {
+    console.log("du bist abgemeldet");    
+} else {
+    console.log("herzlich willkommen");
+}
+
+users.push("Chuck Peterson");
+console.log(users)
+
+
+if (isFull) {
+    console.log("der Raum ist leider voll");    
+} else {
+    console.log("herzlich willkommen");
+}
+
