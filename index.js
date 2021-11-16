ae80a9d08cfd5ad11b8666db3bf0c73563c53663
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-11-16 11:47:00 +0100

    if statements

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 let isFull = false;
-
 if(isFull){
     console.log("der Raum ist offen für neue Nutzer");
 } 
 else {
     console.log("der Raum ist leider voll");
 }
+// )1
+
+if (isFull) {
+    console.log("der Raum ist leider voll");
+} else {
+    console.log("Raum ist offen für neue Nutzer");
+    // 2 )
+const isNutzer = ["John Smith", "Bob Tyler"];
+
+// 3 )
+if (isNutzer == "Chuck Peterson") {
+  
+} else {
+    // 4)
+    isNutzer.push("Chuck Peterson")
+    console.log("Chuck Peterson ist hinzufügen");
+    console.log(isNutzer);
+}
+}
+ // 5)
+ console.log("der Raum ist leider voll")
