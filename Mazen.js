29ba06c4a3ebccc6189c4291833e89479e2986a7
Mazen <mazen.taha88@gmail.com>
2021-11-16 11:46:56 +0100

    My lösung

---
+++ Mazen.js
@@ -0,0 +1,16 @@
+let isFull = ("der Raum ist offen für neue Nutzer");
+
+
+// isFull auf true
+// let isFull = true === ("der Raum ist offen für neue Nutzer");
+
+const users = ["John Smith", "Bob Tyler"];
+
+if (isFull) {
+
+    console.log ("Chuck Peterson "+ users);
+
+} else{
+    console.log("der Raum ist leider voll");
+
+}
