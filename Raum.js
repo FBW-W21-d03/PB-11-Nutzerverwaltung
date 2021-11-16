08f3cc778cef716727db4d8b69e2db9df7334225
slavablack <veaceslav.ciornii@magenta.de>
2021-11-16 11:19:57 +0100

    True

---
+++ Raum.js
@@ -0,0 +1,23 @@
+//1
+let isFull=false;
+const Nutzern=["John Smith", "Bob Tayler"];
+if(isFull==false) {
+Nutzern.push("Chuck Peterson")
+
+     console.log(Nutzern)
+ }
+else{
+    console.log("Der Raum ist leider voll")
+};
+//2
+let istFull=true;
+const Nutzern2=["John Smith", "Bob Tayler"];
+
+if(istFull) { console.log("Der Raum ist leider voll")
+
+
+    console.log(Nutzern2)
+}
+else{
+   Nutzern2.push("Chuck Peterson")
+};
