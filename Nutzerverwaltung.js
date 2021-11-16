86ec3639cd070c6d240593bbf5fed51dd7f498b7
Sharfuddin Ahmed <Sharfuddinahmed07@gmail.com>
2021-11-16 13:32:08 +0100

    its Done

---
+++ Nutzerverwaltung.js
@@ -0,0 +1,24 @@
+
+  
+// 1.)
+
+let isFull = false; // Raum ist offen für neue Nutzer
+
+// Aufgabenstellung - Teil 2
+// isFull = true;
+
+// 2.)
+let users = ["John Smith", "Bob Tyler"];
+
+// 3.)
+if (isFull == false) {
+
+// 4.)
+
+  users.push("Chuck Peterson");
+  console.log(users);
+} else {
+
+  // 5.)
+  console.log("Raum ist  voll!");
+}
