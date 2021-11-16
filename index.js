b402cbd905d76c110658a407a860241ec5afbb60
Mak513 <osei.makangu@hotmail.com>
2021-11-16 11:11:15 +0100

    Osei

---
+++ index.js
@@ -0,0 +1,10 @@
+let isFull = false;
+const users = ["John Smith", "Bob Tyler"];
+if (isFull ==false){
+    users.push("Chuck Peterson");
+    console.log(users);
+}
+    else(isFull==true){
+        console.log("In dem Raum sind", users.length, "Personen, leider ist kein Platz mehr vorhanden");
+    
+    }
