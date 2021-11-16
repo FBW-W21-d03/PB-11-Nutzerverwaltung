2797bfe965ad37ef0cd024f225a0db4530a65afc
aisha2000-hub <aisha.noori2000@gmail.com>
2021-11-16 11:41:07 +0100

    Die lösung von Aisha

---
+++ index.js
@@ -0,0 +1,20 @@
+// 1)
+let isFull = false;
+// let isFull = true;
+
+if (isFull == false){
+
+    console.log("der Raum ist offen für neue Nutzer")
+}
+
+//  2)
+const users = ["John Smith" , "Bob Tyler"];
+
+// 3 -4 -5)
+if (isFull == false){
+     users.push("Chuck Peterson")
+      console.log(users)
+    } else{
+        console.log("der Raum is leider voll.")
+    }
+
