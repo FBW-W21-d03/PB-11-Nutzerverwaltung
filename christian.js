bcb4adc9c15448b4b861d1450435e62cef227bc9
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2021-11-16 10:54:41 +0100

    Lösung von Christian

---
+++ christian.js
@@ -0,0 +1,18 @@
+// 1.)
+let isFull = false; // Raum ist offen für neue Nutzer
+
+// Teil 2
+isFull = true;
+
+// 2.)
+let users = ["John Smith", "Bob Tyler"];
+
+// 3.)
+if (isFull == false) {
+  // 4.)
+  users.push("Chuck Peterson");
+  console.log(users);
+} else {
+  // 5.)
+  console.log("Raum ist leider voll!");
+}
