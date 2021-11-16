7438f9fd31295c9d0742742afe7392839229c63b
sprotte <metatron1972@live.de>
2021-11-16 11:28:40 +0100

    sprottre

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const nutzerNamen = ["john smith", "bob tylor"];
-let  isFull  =  false; 
-console.log(typeof true); 
-if(isFull){console.log("ist voll")}else{nutzerNamen.push("chuck peterson");}
+const nutzerNamen = ["john smith", "bob tylor"]; /* Die const -Deklaration erstellt eine Konstante */
+let  isFull  =  false;                           /* let ist eine Variabel */
+console.log(typeof true);                        /* console.log gibt eine Nachricht als String mit Variablen in die Consolen */
+if(isFull){console.log("ist voll")}else{nutzerNamen.push("chuck peterson");}    /* Die if-Anweisung führt Anweisungen aus, wenn eine bestimmte Bedingung zu true ausgewertet wird. */
 console.log();
 console.log(nutzerNamen);
