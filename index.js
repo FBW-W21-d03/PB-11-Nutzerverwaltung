60254f428ec2c6ebc36dc6f4f957c52ecca57078
sprotte <metatron1972@live.de>
2021-11-16 11:21:49 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,6 @@
+const nutzerNamen = ["john smith", "bob tylor"];
+let  isFull  =  false; 
+console.log(typeof true); 
+if(isFull){console.log("ist voll")}else{nutzerNamen.push("chuck peterson");}
+console.log();
+console.log(nutzerNamen);
