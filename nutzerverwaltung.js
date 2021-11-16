cd7006801636e020d489ce046c6eb6bdd3dbd1cb
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2021-11-16 11:23:28 +0100

    orhan's lösung

---
+++ nutzerverwaltung.js
@@ -0,0 +1,18 @@
+let isFull = false;
+const users = ["John Smith", "Bob Tyler"];
+
+if (isFull == false) {
+    users.push("Chuck Peterson");
+    console.log("Aufgabe 1 = false )", users);
+} else {
+    console.log("Aufgabe 2 = true ) der Raum ist leider voll");
+}
+
+isFull = true;
+
+if (isFull == false) {
+    users.push("Chuck Peterson");
+    console.log("Aufgabe 1 = false )", users);
+} else {
+    console.log("Aufgabe 2 = true ) der Raum ist leider voll");
+}
