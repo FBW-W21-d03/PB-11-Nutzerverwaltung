29a6d5f4f1f0238ff29f78d1f7c70edad8544f8e
H94ssein44 <alali.52@yahoo.com>
2021-11-16 11:38:42 +0100

    done

---
+++ index.js
@@ -0,0 +1,21 @@
+let isFull = true;
+ isFull = false;
+
+
+ isFull = false;
+ console.log ("der Raum ist offen für neue Nutzer")
+
+ const Nutzern=["John Smith","Bob Tyler"]
+
+
+ if ( isFull == false  ){
+     Nutzern.push ("Chuck Peterson")
+     console.log ( Nutzern)
+ }
+
+
+if (isFull== true) {
+    console.log("Chuck Peterson");
+} else {
+    console.log("der Raum ist leider voll");
+}
