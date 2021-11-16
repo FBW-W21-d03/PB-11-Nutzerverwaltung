9ca3d052b01802ca1e611b287a7ccd0d9158f200
kawanaza <kawanbabani@gmail.com>
2021-11-16 11:38:27 +0100

    Done mit zwite augf gabe

---
+++ index.js
@@ -0,0 +1,19 @@
+let isFull = false;
+
+if(isFull == false ){
+   
+    console.log("der Raum ist offen für neue Nutzer")
+} else {
+   
+    console.log(isFull = true);
+}
+
+let Nutzern = ["John Smith","Bob Tyler"];
+console.log(Nutzern);
+
+if (isFull == false) {
+   isFull.push("Checke Person");
+   console.log("false Name Check again ");
+} else {
+    console.log(" der raum ist voll");
+}
